7e3bc1ebccb8a6e8b1ca95bdfb54b2eca76aa070
Nit Wu <NittayaK80@Gmail.com>
2023-03-02 08:12:16 -0500

    files created

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=093e40f128be9d59f574e243c76c0986
+// current weather
+
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=093e40f128be9d59f574e243c76c0986
+// direct geocode
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=093e40f128be9d59f574e243c76c0986
+// 5-day weather forcaste
