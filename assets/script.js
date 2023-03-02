e1dca8a1b6d851636ecaa832f47b96454ea57ef0
Nit Wu <NittayaK80@Gmail.com>
2023-03-02 13:44:30 -0500

    test api-key in js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-
+var weather = {
+    "apiKey": "093e40f128be9d59f574e243c76c0986",
+    fetchWeather:function(){
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=093e40f128be9d59f574e243c76c0986"
+        ).then((Response) => Response.json())
+        .then((data) => console.log(data))
+    }
+}
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=093e40f128be9d59f574e243c76c0986
 // current weather
 
