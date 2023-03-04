68055ffe3d211d9ce012dab2b0d270831668d23c
Nit Wu <NittayaK80@Gmail.com>
2023-03-04 10:52:15 -0500

    modified js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,25 @@
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=093e40f128be9d59f574e243c76c0986
+// eventListener
+var app = {
+    init: () => {
+        document
+        .getElementById("search-btn")
+        .addEventListener("click",app.fetchWeather);
+
+    }
+}
+// current weather
 var weather = {
     "apiKey": "093e40f128be9d59f574e243c76c0986",
-    fetchWeather:function(){
+    fetchWeather:function(city){
+        // fetch the weather
         fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=093e40f128be9d59f574e243c76c0986"
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey
         ).then((Response) => Response.json())
         .then((data) => console.log(data))
     }
 }
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=093e40f128be9d59f574e243c76c0986
-// current weather
-
 
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=093e40f128be9d59f574e243c76c0986
 // direct geocode
