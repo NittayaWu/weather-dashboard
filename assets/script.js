1a1ce606d08bae23a3b6064279ad747ef6950399
Nit Wu <NittayaK80@Gmail.com>
2023-03-05 13:10:03 -0500

    modified js

--- assets/script.js
+++ assets/script.js
@@ -11,15 +11,24 @@ var app = {
 }
 // current weather
 var weather = {
-    "apiKey": "093e40f128be9d59f574e243c76c0986",
+    apiKey: "093e40f128be9d59f574e243c76c0986",
     fetchWeather:function(city){
         // fetch the weather
         fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey
-        ).then((Response) => Response.json())
-        .then((data) => console.log(data))
-    }
-}
+        "https://api.openweathermap.org/data/2.5/weather?q=" + 
+        city 
+        + "&appid="
+        + this.apiKey
+        )
+            if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return response.json();
+          }
+          .then((data) => this.displayWeather(data));
+    },
+
 
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=093e40f128be9d59f574e243c76c0986
 // direct geocode
